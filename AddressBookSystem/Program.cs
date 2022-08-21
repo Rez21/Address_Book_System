d963d756f0eb10376f93d489c831dbcbdc4bc36c
Prasad <banprasad21@gmail.com>
2022-08-21 22:19:57 +0530

    [Raghav] Add: adding contact to addressbook by user input

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,20 +1,30 @@
 ﻿using AddressBookSystem;
 using System;
 
-namespace Address_Book
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome To Address Book");
+Console.WriteLine("Welcome To Address Book");
+
+Console.WriteLine("--Add Contact Details--");
 
 
-            AddressBookSystem.ContactOperation contactOperation = new ContactOperation();
-            contactOperation.addContact("Shahrukh", "Khan", "Bandra", "Mumbai", "Maharashtra", "123345", "123456789", "srk@gmail.com");
-            contactOperation.showList();
+Console.WriteLine("First Name:");
+string firstName = Console.ReadLine();
+Console.WriteLine("Last Name:");
+string lastName = Console.ReadLine();
+Console.WriteLine("Address:");
+string address = Console.ReadLine();
+Console.WriteLine("City:");
+string city = Console.ReadLine();
+Console.WriteLine("State:");
+string state = Console.ReadLine();
+Console.WriteLine("Zip Code:");
+string zip = Console.ReadLine();
+Console.WriteLine("Phone Number:");
+string phoneNumber = Console.ReadLine();
+Console.WriteLine("Email Address:");
+string email = Console.ReadLine();
 
-            Console.ReadKey();
-        }
-    }
-}
+ContactOperation contactOperation = new ContactOperation();
+contactOperation.addContact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+contactOperation.showList();
+
+Console.ReadKey();
