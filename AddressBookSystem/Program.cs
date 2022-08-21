c3d1e25d1d58d8e95991a5847a33039fe17bc0b9
Prasad <banprasad21@gmail.com>
2022-08-21 22:04:59 +0530

    [Raghav] Add: created address book system with initial contact display

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using AddressBookSystem;
+using System;
+
+namespace Address_Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book");
+
+
+            AddressBookSystem.ContactOperation contactOperation = new ContactOperation();
+            contactOperation.addContact("Shahrukh", "Khan", "Bandra", "Mumbai", "Maharashtra", "123345", "123456789", "srk@gmail.com");
+            contactOperation.showList();
+
+            Console.ReadKey();
+        }
+    }
+}
